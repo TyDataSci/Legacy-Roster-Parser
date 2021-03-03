42a1f56779c0fdfb10793156e13c2bd908b25c4d
Ty Sanders <sandetab2@gmail.com>
2021-03-03 06:19:17 -0600

    Add files via upload
    Replaced all occurrences previously implemented with namespace std;

---
+++ ClassRoster/ClassRoster/main.cpp
@@ -0,0 +1,25 @@
+#include "student.h"
+#include "roster.h"
+#include "degree.h"
+
+
+
+
+int main() {
+
+	const std::string studentData[] =
+
+	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+		"A5,Tyler,Sanders,tsanders140@wgu.edu,29,17,14,25,SOFTWARE" };
+
+	roster roster;
+	roster.parse(studentData);
+	roster.printAll();
+	roster.printInvalidEmails();
+	roster.printAverageDaysInCourse();
+	roster.printByDegreeProgram(DegreeProgram::SOFTWARE);
+	roster.remove("A3");
+	roster.printAll();
+	roster.remove("A3");
+}
---
+++ ClassRoster/ClassRoster/student.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "degree.h"
+
+class student
+{
+public:
+static const int NUM_SIZE = 3;
+
+
+private:
+	std::string studentId;
+	std::string firstName;
+	std::string lastName;
+	std::string emailAddress;
+	int age;
+	int daysToComplete[NUM_SIZE] = {0,0,0};
+	DegreeProgram degree;
+
+public:
+	//constructors
+	student();
+	student(std::string ID, std::string firstName, std::string lastName, std::string email, int age, int days[], DegreeProgram degree);
+	//deconstructor
+	~student();
+	//getters
+	std::string GetId();
+	std::string GetFirstName();
+	std::string GetLastName();
+	std::string GetEmail();
+	int GetAge();
+	int* GetDaysToComplete();
+	std::string GetDegree();
+	
+
+
+	//setters
+	void SetId(std::string ID);
+	void SetFirstName(std::string firstName);
+	void SetLastName(std::string lastName);
+	void SetEmail(std::string email);
+	void SetAge(int age);
+	void SetDaysToComplete(int *days);
+	void SetDegree(DegreeProgram degree);
+
+	//methods
+	void print();
+	std::string DegreeToString();
+};
+
